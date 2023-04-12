20b1a5393c533ff0207760796d223d4a1fff0e65
Kim Tolberg Ørsøe <kor@ida.dk>
2023-04-12 10:15:17 +0200

    add react to components

--- Components/AppShell.js
+++ Components/AppShell.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {MantineProvider} from "@mantine/core"
 
 const AppShell = ({children}) => {
--- Components/AppShellWithChildren.js
+++ Components/AppShellWithChildren.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {Button, MantineProvider} from "@mantine/core"
 
 const AppShellWithChildren = () => {
