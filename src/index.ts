4fef2e6f8d1421c6921416b7d7d7824cfe6c7c21
Kim Tolberg Ørsøe <kor@ida.dk>
2023-04-12 15:45:52 +0200

    rtl test 3

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {default as AppShellWithChildren} from "./AppShellWithChildren";
 export {default as AppShell} from "./AppShell";
+export {rtlCache} from "./rtl-cache";
+export * from "@mantine/core";
